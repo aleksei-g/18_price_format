96cea1540c0374cf478ca19b837b3c766fc5afc3
aleksei-g <lexa_prorock@mail.ru>
2016-10-18 10:58:43 +0300

    написан скрипт и тесты

--- format_price.py
+++ format_price.py
@@ -1,5 +1,28 @@
+import argparse
+import re
+
+def create_parser():
+    parser = argparse.ArgumentParser(description="Скрипт выполняет "
+                                                 "форматирование строки "
+                                                 "цены товара.")
+    parser.add_argument('price', metavar='ТОП',
+                        help='Форматируемая строка цены товара.')
+    return parser
+
+
 def format_price(price):
-    pass
+    if not isinstance(price,(float, int, str)):
+        raise TypeError('price type must be a string, int or float.')
+    if not re.match('\d+\.?\d+$', str(price).strip()):
+        raise ValueError('Error format: value must contain only numbers and '
+                         'symbol "." (like int or float type)')
+    price = float(price)
+    if round(price, 2) % 1 == 0:
+        return '{:,.0f}'.format(price).replace(',', ' ')
+    else:
+        return '{:,.2f}'.format(price).replace(',', ' ')
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    namespace = parser.parse_args()
+    print(format_price(namespace.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,36 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class PriceFormatTestCase(unittest.TestCase):
+    def test_correct__float_value(self):
+        input_pirce = 1999.99
+        formatted_price = '1 999.99'
+        self.assertEqual(format_price(input_pirce), formatted_price)
+    def test_correct_int_value(self):
+        input_pirce = 199999
+        formatted_price = '199 999'
+        self.assertEqual(format_price(input_pirce), formatted_price)
+    def test_correct_str_value(self):
+        input_pirce = '  199.999  '
+        formatted_price = '200'
+        self.assertEqual(format_price(input_pirce), formatted_price)
+    def test_incorrect_str_value(self):
+        input_pirce = '-199.999'
+        with self.assertRaises(ValueError):
+            format_price(input_pirce)
+    def test_incorrect_str_value_2(self):
+        input_pirce = '199,999'
+        with self.assertRaises(ValueError):
+            format_price(input_pirce)
+    def test_incorrect_flaot_value(self):
+        input_pirce = -199.999
+        with self.assertRaises(ValueError):
+            format_price(input_pirce)
+    def test_type_err(self):
+        input_pirce = [199,999]
+        with self.assertRaises(TypeError):
+            format_price(input_pirce)
+
+
+if __name__ == '__main__':
+    unittest.main()
